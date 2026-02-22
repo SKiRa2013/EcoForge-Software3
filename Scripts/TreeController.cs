694af70627742d4c45554f9235d16e33b212e865
S-KiRa <=>
2026-02-22 02:55:23 -0500

    SCRUM-61 2026-02-22 2:55,-05:00. Tree planting mechanic made. TreePlantController.cs within 'background' object, TreeController.cs within 'influence_zone' object in Tree prefab.

---
+++ Scripts/TreeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class TreeController : MonoBehaviour {
+    public bool isSeedPlanted;
+    bool isValid;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start() {
+        isValid = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isSeedPlanted) return;
+        
+        timer += Time.deltaTime;
+
+        if (timer >= 0.2f) {
+            timer = 0f;
+
+            if (!isValid) {
+                Destroy(gameObject.transform.parent.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (isValid) return;
+
+        isValid = other.CompareTag("TreeArea") && other.transform.parent != transform.parent;
+    }
+}
---
+++ Scripts/TreePlantController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreePlantController : MonoBehaviour {
+    float timer;
+    public float plantCooldown = 30f;
+    public GameObject treePrefab;
+    
+    Vector3 GetMousePos() {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+
+        return mousePos;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timer += Time.deltaTime;
+
+        if (timer >= plantCooldown) {
+            if (Input.GetKeyDown("space")) {
+                Instantiate(treePrefab, GetMousePos(), Quaternion.identity);
+                timer = 0f;
+            } else if (Input.GetKeyDown(KeyCode.F)) {
+                GameObject newTree = Instantiate(treePrefab, GetMousePos(), Quaternion.identity);
+                newTree.GetComponentInChildren<TreeController>().isSeedPlanted = true;
+                timer = 0f;
+            }
+        }
+    }
+}
